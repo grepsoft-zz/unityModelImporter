819765c5e2ad1bc81667e861c615e6a123eb3745
Grepsoft <45298821+grepsoft@users.noreply.github.com>
2020-04-20 19:56:34 -0400

    initial

---
+++ GSModelImporter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class GSModelImporter : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+        ModelImporter modelImporter = assetImporter as ModelImporter;
+
+        // Model Settings
+        modelImporter.importCameras = false;
+        modelImporter.importLights = false;
+
+        // Rig settings
+        modelImporter.animationType = ModelImporterAnimationType.Human;
+
+        // Animatino Settings
+        modelImporter.importAnimation = false;
+
+
+        // Material
+        if(!modelImporter.ExtractTextures(@"Assets/Textures"))
+        {
+            Debug.LogError("Failed to extract textures");
+        }
+    }
+}
